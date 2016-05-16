8fbb91db0f52ef1cb26cd743e94d68a6614dacb5
tuni93 <funestunes@hotmail.com>
2016-05-16 19:11:24 -0300

    subo primer versión del codigo

---
+++ codigo/trabajo_practico2.asm
@@ -0,0 +1,107 @@
+.INCLUDE "M328PDEF.INC"
+.ORG $00
+JMP PROGRAMA // Reset
+
+PROGRAMA:
+	LDI R16,HIGH(RAMEND)
+	OUT SPH,R16
+	LDI R16,LOW(RAMEND)
+	OUT SPL,R16
+	LDI R16,0xFF ; R16 a unos
+	OUT DDRD,R16 ;Configura todo el puerto D como salidas
+	OUT DDRB,R16 ;Configura todo el puerto B como salidas
+	LDI R16,0 ;
+	OUT DDRC,R16 ;Configuro todo el puerto C como salidas
+	;Configuracion del ADC
+	LDS R16,ADMUX;
+	ORI R16,((1<<REFS0)|(1<<MUX2)|(1<<MUX0));
+	STS ADMUX, R16; Se configura AVcc como la referencia 	
+	LDS R16,ADCSRA;
+	ORI R16,((1<<ADEN)|(1<<ADPS1)|(1<<ADPS0)); Seteo el valor de division del clk 1M/8
+	STS ADCSRA, R16; Habilito el adc
+	
+CONFIGURACION_PWM:
+	LDI R25,0
+	ORI R25,((1<<WGM01)|(1<<WGM00)|(1<<COM0B1))
+	STS TCCR0A,R25
+	LDI R25,0
+	ORI R25,((1<<CS00))
+	STS TCCR0B,R25
+	LDI R25,0XFF
+	OUT OCR0B,R25
+ENCIENDO:
+	CALL RETARDO;
+	LDS R16,ADCSRA;
+	ORI R16,(1<<ADSC)
+	STS ADCSRA,R16 ;
+CONVERSION:
+	LDS R24,ADCSRA ;
+	SBRC R24,4;
+	RJMP CONVERSION;
+	LDS R24,ADCSRA;
+	ORI R24, (1<<ADIF)
+	STS ADCSRA,R24
+	LDI ZL,LOW(VECTOR_LUZ<<1)
+	LDI ZH,HIGH(VECTOR_LUZ<<1)
+	LPM R16,Z+;
+	LDS R18,ADCL;
+	LDS R19,ADCH;
+	LDI R23,0 ;
+	OUT PORTB,R23
+	OUT PORTD,R23
+	LSR R19;%Por relacion llevo el espacios de 1023ptos a 255ptos dividiendo en 4
+	ROR R18;
+	LSR R19
+	ROR R18;
+	;ALL PWM;
+	;ALL RETARDO_PWM;
+	CP R18,R16;%Comparo con la maxima intensidad para la que esta todo apagado.
+	BRLO ENCIENDO
+	SBI PORTB,0 ;Hay poca oscuridad pero prendo 1 Y apago 2
+	CBI PORTD,6
+	CBI PORTD,7
+	LPM R16,Z+;
+	CP R18,R16
+	BRLO ENCIENDO
+	SBI PORTB,0
+	SBI PORTD,7 ;Enciendo dos y apago 1
+	CBI PORTD,6
+	LPM R16,Z;
+	CP R18,R16
+	BRLO ENCIENDO
+	SBI PORTD,6 ;%Enciendo 3 
+	SBI PORTD,7
+	SBI PORTB,0
+	
+	RJMP ENCIENDO
+
+
+
+RETARDO:
+	LDI R20,132
+	LDI R21,150
+	LDI R22,8
+LOOP:
+	DEC R20
+	BRNE LOOP
+	DEC R21
+	BRNE LOOP
+	DEC R22
+	BRNE LOOP
+	RET
+
+RETARDO_PWM:
+	LDI R20,255
+LOOP_PWM:
+	DEC R20
+	BRNE LOOP_PWM
+	NOP
+	RET
+
+PWM:
+	OUT OCR0B,R18;
+	RET
+
+FIN:JMP FIN
+
+VECTOR_LUZ:			.db 64, 128 , 192
