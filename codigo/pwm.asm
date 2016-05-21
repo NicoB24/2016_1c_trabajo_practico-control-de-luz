9af83f6414041c178c3c78ee621a532120c2d31c
tuni93 <funestunes@hotmail.com>
2016-05-20 23:33:14 -0300

    modifique el archivo del pwm para simplificarlo

--- codigo/pwm.asm
+++ codigo/pwm.asm
@@ -1,25 +1,13 @@
 .INCLUDE "M328PDEF.INC"
 .ORG $00
 JMP PROGRAMA // Reset
-
-
-
+;Configuracion Fast Pwm, duty cicle 50%
 PROGRAMA:
 	LDI R16,HIGH(RAMEND)
 	OUT SPH,R16
 	LDI R16,LOW(RAMEND)
 	OUT SPL,R16
-	LDI R16,0xFF ; R16 a unos
-	OUT DDRD,R16 ;Configura todo el puerto D como salidas
-	OUT DDRB,R16 ;Configura todo el puerto B como salidas
-	LDI R16,0 ;
-	OUT DDRC,R16 ;Configuro todo el puerto C como entradas
-	
-
-
-
-
-
+	SBI DDRD,6 ;El pin del pwm es (PCINT22/OC0A/AIN0) PD6
 
 ;CONFIGURACION_PWM:
 	LDI R25,128
@@ -30,18 +18,6 @@ PROGRAMA:
 	LDI R25,0
 	ORI R25,((0<<CS00)|(1<<CS01)|(0<<CS02))
 	OUT TCCR0B,R25
-	
-	
-	;LDI R25,40
-	;STS TCNT0,R25
-	;LDI R25,0
-	;ORI R25,((1<<WGM00)|(1<<COM0A1)|(1<<COM0A0))
-	;STS TCCR0A,R25
-	;LDI R25,0
-	;ORI R25,((0<<CS00)|(0<<CS01)|(0<<CS02))
-	;STS TCCR0B,R25
-	
-	
 	
 
 FIN:rjmp FIN;
